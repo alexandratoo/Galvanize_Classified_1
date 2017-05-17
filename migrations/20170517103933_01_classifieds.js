6931cc49b54892898b19ba368d0b3d32e5759e86
Alexandra Tooley <sandratooley@rocketmail.com>
2017-05-17 11:05:17 -0600

    fixing routes and why is title not the same?

--- migrations/20170517103933_01_classifieds.js
+++ migrations/20170517103933_01_classifieds.js
@@ -1,10 +1,10 @@
 
 
-'use strict'
+// 'use strict'
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('classifieds', table => {
     table.increments()
-    table.string('title', 255).notNullable()
+    table.string('title', 255)
     table.string('description', 255).notNullable()
     table.decimal('price').notNullable()
     table.string('item_image', 255).notNullable()
