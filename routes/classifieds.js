16aa15f367be4d24ce22fae77444ef38f7373a18
Alexandra Tooley <sandratooley@rocketmail.com>
2017-05-17 11:00:56 -0600

    fixed migration

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -6,5 +6,23 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR CODE HERE
+router.get('/classifieds', function(req, res){
+    knex('classifieds')
+      .orderBy('title')
+       .then(function(data){
+          res.send(humps.camelizeKeys(data))
+      })
+ })
+router.get('/classifieds/:id', function(req, res){
+  knex('classifieds')
+})
+ router.post('/classifieds', function(req, res){
+   knex
+ })
+ router.patch('/classifieds/:id', function (req, res){
 
+ })
+ router.delete('classifieds/:id', function (req, res){
+
+ })
 module.exports = router;
