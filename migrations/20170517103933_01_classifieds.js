96981faf7304dc45ddbd15cffe534e9bd4827995
Alexandra Tooley <sandratooley@rocketmail.com>
2017-05-17 11:03:37 -0600

    fixed seed

--- migrations/20170517103933_01_classifieds.js
+++ migrations/20170517103933_01_classifieds.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('classifieds', table => {
     table.increments()
     table.string('title', 255).notNullable()
-    table.string("description", 255).notNullable()
+    table.string('description', 255).notNullable()
     table.decimal('price').notNullable()
     table.string('item_image', 255).notNullable()
     table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
--- routes/classifieds.js
+++ routes/classifieds.js
@@ -1,28 +1,66 @@
-
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex')
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 // YOUR CODE HERE
-router.get('/classifieds', function(req, res){
-    knex('classifieds')
-      .orderBy('title')
-       .then(function(data){
-          res.send(humps.camelizeKeys(data))
-      })
- })
-router.get('/classifieds/:id', function(req, res){
+router.get('/', (req, res, next) => {
   knex('classifieds')
+    .select(['id', 'title', 'description', 'price', 'item_image'])
+    .then((data) => {
+      res.send(data)
+    })
 })
- router.post('/classifieds', function(req, res){
-   knex
- })
- router.patch('/classifieds/:id', function (req, res){
 
- })
- router.delete('classifieds/:id', function (req, res){
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  knex('classifieds')
+    .where('id', id)
+    .select(['id', 'title', 'description', 'price', 'item_image'])
+    .then((data) => {
+      res.send(data[0])
+    })
+})
 
- })
+router.post('/', (req, res, next) => {
+  const newPost = {
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    item_image: req.body.item_image
+  }
+  knex('classifieds')
+    .returning(['id', 'title', 'description', 'price', 'item_image'])
+    .insert({title: newPost.title, description: newPost.description, price: newPost.price, item_image: newPost.item_image})
+    .then((data) => {
+      res.send(data[0])
+    })
+})
+
+router.patch('/:id', (req, res, next) => {
+  const id = req.params.id
+  const body = req.body
+  knex('classifieds')
+    .returning(['id', 'title', 'description', 'price', 'item_image'])
+    .where('id', id)
+    .update({title: body.title, description: body.description, price: body.price, item_image: body.item_image})
+    .then((data) => {
+      res.send(data[0])
+    })
+})
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  const body = req.body
+  knex('classifieds')
+    .returning(['id','title', 'description', 'price', 'item_image'])
+    .where('id', id)
+    .del()
+    .then((data) => {
+      res.send(data[0])
+    })
+})
+
 module.exports = router;
